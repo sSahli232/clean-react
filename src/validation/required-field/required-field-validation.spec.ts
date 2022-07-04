da0a57f1d62e8512f8ecb83d00bec07aba9cf260
sSahli232 <sahli.selim@gmail.com>
2022-07-04 22:55:44 +0200

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { ExtendedFieldError } from '@/validation/errors'
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
   test('should return error is field is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new ExtendedFieldError())
+  })
+
+  test('should return falsy is field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
